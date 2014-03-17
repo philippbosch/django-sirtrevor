5bf5b080abfcb78a3b0eb0f3dcde297aced60b65
Philipp Bosch <hello@pb.io>
2014-03-17 20:51:05 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='django-sirtrevor',
-    version= '0.1.2',
+    version= '0.1.3',
     packages=['sirtrevor'],
     include_package_data=True,
     license='MIT License',
